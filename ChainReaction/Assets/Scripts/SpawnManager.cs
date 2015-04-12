2d000259ffe5e9065e7202646d80dcd7c7378582
sykuo <sykuo@uci.edu>
2015-04-11 20:51:53 -0700

    spawnManager

--- ChainReaction/Assets/Scripts/SpawnManager.cs
+++ ChainReaction/Assets/Scripts/SpawnManager.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class SpawnManager : MonoBehaviour {
+	// counter for how many mobs are still live
+	public int mobCounter; 
+	public GameObject mob1;
 
+
+	public float maxBoundaryX;
+	public float maxBoundaryY;
+	public float minBoundaryX;
+	public float minBoundaryY;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(mobCounter < 10){
+			SpawnMob ();
+			mobCounter+=1;
+		}
+	}
+	void SpawnMob(){
+		Instantiate(mob1, new Vector3(Random.Range (minBoundaryX,maxBoundaryX), Random.Range(minBoundaryY,maxBoundaryY), 0), Quaternion.Euler(0, 0, 0));
 	}
 }
