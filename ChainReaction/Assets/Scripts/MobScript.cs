e25dce0124f291c9499c28d008a09ad90e6f5980
sykuo <sykuo@uci.edu>
2015-04-11 19:00:00 -0700

    added MobScript

---
+++ ChainReaction/Assets/Scripts/MobScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobScript : MonoBehaviour {
+	public GameObject mob1;
+	public float speed;
+	public float maxBoundaryX;
+	public float maxBoundaryY;
+	public float minBoundaryX;
+	public float minBoundaryY;
+	//random position on the map 
+	public Vector3 targetPosition;
+	public Vector2 direction;
+
+
+	// Use this for initialization
+	void Start () {
+		targetPosition.x = Random.Range (minBoundaryX,maxBoundaryX);
+		targetPosition.y = Random.Range (minBoundaryX, maxBoundaryY);
+		targetPosition.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position,targetPosition) < .1f){
+			targetPosition.x = Random.Range (minBoundaryX,maxBoundaryX);
+			targetPosition.y = Random.Range (minBoundaryX, maxBoundaryY);
+			targetPosition.z = 0;
+		}
+
+	}
+
+	void FixedUpdate(){
+		direction = (targetPosition - this.transform.position).normalized;
+		this.transform.GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+}
--- ChainReaction/Assets/Scripts/SlideController.cs
+++ ChainReaction/Assets/Scripts/SlideController.cs
@@ -2,31 +2,75 @@
 using System.Collections;
 
 public class SlideController : MonoBehaviour {
-	public float speed;
+	/*public float speed;
 	private GameObject platformprefab;
 	private GameObject lastCreatedPlatform;
 	private Vector3 lastPlatformPosition;
 	public Vector3 offset;
+	*/
+	public GameObject jumpTerrain;
+	public GameObject platformTerrain;
+	public GameObject player;
+	public float startSpawnPosition = 11.2f;
+	public int spawnYPos = 0;
+	int randomChoice;
+	float lastPosition;
+	//death reference
+	GameObject spike;
+	GameObject platform;
+	GameObject theplayer;
+	bool canSpawn = true;
 
-	void Awake()
-	{
-		speed = 6f;
-		lastPlatformPosition = new Vector3 (0, 0, 0);
-		offset = new Vector3 (1, 0, 0);
-	}
+
+
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("CreatePlatform", 0, 1f);
+		//InvokeRepeating ("CreatePlatform", 0, 1f);
+		lastPosition = startSpawnPosition;
+
+		spike = GameObject.Find ("Spikes");
+		//platform = GameObject.FindGameObjectWithTag ("plat");
+		theplayer = GameObject.Find ("Cube");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(-1 * Time.deltaTime, 0, 0 );
+		//if this platform's position is passed the spike, spawn another 
+		if (gameObject.transform.position.x <= spike.transform.position.x - 3 && canSpawn == true){ //&& theplayer.transform.x >= lastPosition-3) {
+			canSpawn = false;
+			randomChoice = Random.Range(1,10);
+			SpawnTerrain(randomChoice);
+			Destroy (gameObject);
+		}
+		transform.Translate(-10 * Time.deltaTime, 0, 0 );
 
 	}
-	void CreatePlatform()
-	{
-		lastCreatedPlatform = (GameObject)Instantiate (platformprefab, lastPlatformPosition + offset , Quaternion.identity);	
-		lastPlatformPosition = lastCreatedPlatform.transform.position;
+	void SpawnTerrain(int rand){
+		if (rand >= 1 && rand <= 4)
+		{
+			Instantiate(jumpTerrain, new Vector3(lastPosition + 4, spawnYPos, 0), Quaternion.Euler(0, 0, 0));
+			// same as start spawn position as starting terrain
+
+			lastPosition += 8.2f; //this # is random af idk
+		}
+		
+		if (rand >= 5 && rand <= 8)
+		{
+			Instantiate(platformTerrain, new Vector3(lastPosition, spawnYPos, 0), Quaternion.Euler(0, 0, 0));
+			lastPosition += 8.2f;
+		}
+		
+		if (rand >= 9 && rand <= 10)
+
+		{
+			Instantiate(platformTerrain, new Vector3(lastPosition, spawnYPos, 0), Quaternion.Euler(0, 0, 0));
+			lastPosition += 8.2f;
+		}
+		
+
+		canSpawn = true;
+
 	}
+
+
 }
