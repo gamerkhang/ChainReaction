3bd9951c38f30557df31958d6f5cc6b6fe2faf0c
Khang Tran <gamerkhang@gmail.com>
2015-04-16 23:36:59 -0700

    test
    -created/implemented spawnMarker code
    -reverted noshrinking changes to normal
    -made max spawns 20 on 4player map
    -thinking about adding a timer...
    -thinking about adding counters for how much you've killed/run into...
    -thinking about adding the features from the pic...
    -thinking about adding a timer onscreen...

--- ChainReaction/Assets/Scripts/SpawnManager.cs
+++ ChainReaction/Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 public class SpawnManager : MonoBehaviour {
 	// counter for how many mobs are still live
 	public int mobCounter; 
+	public int maxMobs;
+	public GameObject spawnMarker;
 	public GameObject[] mobArray;
 	public List<GameObject> mobList= new List<GameObject>();
 	public float maxBoundaryX;
@@ -11,6 +13,8 @@ public class SpawnManager : MonoBehaviour {
 	public float minBoundaryX;
 	public float minBoundaryY;
 	public float spawnTimer;
+	public float spawnDelay;
+	public float spawnMarkerDelay;
 	// Use this for initialization
 	void Start () {
 		spawnTimer = 0;
@@ -19,16 +23,19 @@ public class SpawnManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		spawnTimer += Time.deltaTime;
-		if(mobCounter < 10&& spawnTimer > 1.5){
-			SpawnMob ();
-			mobCounter++;
+		if((mobCounter < maxMobs) && (spawnTimer > spawnDelay)){
+			StartCoroutine(SpawnMob(spawnTimer));
 			spawnTimer = 0;
 		}
 
 	}
-	void SpawnMob(){
+
+	IEnumerator SpawnMob(float spawnDelay){
 		Vector3 pos = new Vector3 (Random.Range (minBoundaryX, maxBoundaryX),
 		                          Random.Range (minBoundaryY, maxBoundaryY), 0);
+		GameObject spawnPoint = (GameObject) Instantiate(spawnMarker, pos, Quaternion.identity);
+		yield return new WaitForSeconds(spawnDelay);
+		Destroy (spawnPoint);
 		Instantiate (mobArray[Random.Range (0,3)], pos, Quaternion.identity);
 	}
 }
